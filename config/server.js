e5e4c8e19c5355c6c9e86bdc932af704f691df3d
kishorewolfe <kishorewolfeik@gmail.com>
2024-10-12 22:12:24 +0530

    server changes

--- config/server.js
+++ config/server.js
@@ -1,6 +1,13 @@
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: '0.0.0.0',  // Make Strapi accessible externally
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'http://143.110.177.97/'), // Optional, add if you want to access via domain
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '24hectors123'),
+    },
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
